6fd2380d0d62d9df05e473698f2cc403d875a164
surya <surya.mylar@gmail.com>
2016-06-22 11:50:47 -0400

    Fixed logicalOperators,modules,numbers

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,12 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
-  }
+		var module = {};
+		module.name = str2;
+		module.greeting = str1;
+		module.sayIt = function() {
+			return module.greeting + ', ' + module.name;
+		};
+		return module;
+    }
 };
--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,61 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
+        var bitLoc = 1 << (bit-1);
+        if((num & bitLoc) > 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    },
 
-  },
+    base10: function(str) {
+        var num = 0;
+        var j = 0;
+        for(var i = str.length -1; i >= 0; i--) {
+            if(str[j] === '1') {
+                num += Math.pow(2, i);
+            }
+            j++;
+        }
+        return num;
+    },
 
-  base10: function(str) {
+    convertToBinary: function(num) {
+        var binary = num.toString(2);
+        var counter = 8 - binary.length;
+        while (counter > 0) {
+            binary = '0' + binary;
+            counter--;
+        }
+        return binary;
+    },
 
-  },
+    multiply: function(a, b) {
+        var result = a * b;
+        var precisionA = a.toString().split('.');
+        var precisionB = b.toString().split('.');
+        var precision;
+        if(precisionA.length === 1 || precisionB.length === 1)
+        {
+            if(precisionA.length > 1){
+                precision = precisionA[1].length;
+            }
+            else if(precisionB.length > 1) {
+                precision = precisionB[1].length;
+            }
+            else {
+                precision = 0;
+            }
+        }
+        else
+        {
+            precision = precisionA[1].length + precisionB[1].length;
+        }
+        result = result.toPrecision(precision);
+        return parseFloat(result);
 
-  convertToBinary: function(num) {
 
-  },
-
-  multiply: function(a, b) {
-
-  }
+    }
 };
